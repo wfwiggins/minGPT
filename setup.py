cc8d845fe35771dcdfc7c65ea80f50ee58c2df3e
Walter Wiggins, MD, PhD <walterfwiggins@gmail.com>
2021-11-10 20:56:18 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name="minGPT",
+        version="0.0.1",
+        author="Andrej Karpathy",
+        author_email="andrej.karpathy@gmail.com",
+        packages=["minGPT"],
+        package_dir={"minGPT":"minGPT"},
+        url="https://github.com/karpathy/minGPT",
+        license="MIT",
+        install_requires=["numpy >= 1.8",
+                          "torch >= 1.9"]
+)
